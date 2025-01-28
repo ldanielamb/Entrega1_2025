2ce68c22c0dd267d7b045a330971b95526d0f4c2
ldanielam <lamarinb@unal.edu.co>
2025-01-28 17:02:20 -0500

    ejercicio4_versionfinal

--- ejercicios.py
+++ ejercicios.py
@@ -1,10 +1,25 @@
-"""Programa que determine si una lista es palíndrome, 
-una lista es palíndrome si el elemento en la posición i es el 
-mismo de la posición n - 1 - i con n la longitud de la lista"""
-lista_1 = ["hola", "si", "si", "hola"] #Creación de una lista palíndrome
-for i in range (len(lista_1)): #Se generarán números de 0 hasta el valor de la longitud de la lista
-    if lista_1[i] == lista_1[(len(lista_1) - 1 - i)]: #Se recurre al elemento en la posición indicada por el número i, si este es igual al elemento indicado en la posición n-1-i, es verdadero
-        print("Su lista es palíndrome") #Ej: i = a 0, luego, se evalúa la posición 0 de la lista. Si el elemento es igual al de la posición (4 - 1 - 0), es palíndrome
-    else: 
-        print("Su lista no es palíndrome")
-print(len(lista_1))
+def crear_lista():
+    l = []
+    longitud = int(input("Digite la longitud de su lista: "))
+    while longitud <= 2:
+        print("La longitud de un palíndrome no puede ser menor a dos elementos")
+        longitud = int(input("Digite la longitud de su lista: "))
+    for i in range(longitud):
+        elementos = input(f"Digite su elemento número {i + 1}: ")
+        l1 = l.append(elementos)
+    return l 
+
+print("-----------------------Programa que evalúa si la lista es o no palíndrome-----------------------")
+lista_1 = crear_lista()
+n = len(lista_1)
+es_palindrome = True
+for i in range (n): #Se generarán números de 0 hasta el valor de la longitud de la lista
+    if lista_1[i] != lista_1[(len(lista_1) - 1 - i)]: #Se recurre al elemento en la posición indicada por el número i, si este es igual al elemento indicado en la posición n-1-i, es verdadero
+        es_palindrome = False
+        break
+if es_palindrome:
+    print("Su lista es palíndrome")
+elif n <= 2:
+    print("Un palíndrome debe tener más de dos elementos")
+else:
+    print(f"Su lista {lista_1} no es palíndrome")
