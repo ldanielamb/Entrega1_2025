9a32b4299de448ac65396a5b15a69124d119c382
ldanielam <lamarinb@unal.edu.co>
2025-01-28 23:08:05 -0500

    optimizacion_final

--- ejercicios.py
+++ ejercicios.py
@@ -1,49 +1,44 @@
-def crear_lista4():
+def crear_lista(longitud_minima=0):
     l = []
     longitud = int(input("Digite la longitud de su lista: "))
-    while longitud <= 2:
-        print("La longitud de un palíndrome no puede ser menor a dos elementos")
+    while longitud <= longitud_minima:
+        print(f"La longitud de la lista debe ser mayor que {longitud_minima} elementos")
         longitud = int(input("Digite la longitud de su lista: "))
-    for i in range(longitud):
-        elementos = input(f"Digite su elemento número {i + 1}: ")
-        l1 = l.append(elementos)
-    return l
-def crear_lista3():
-    l = []
-    longitud = int(input("Digite la longitud de su lista: "))
     for i in range(longitud):
         elementos = input(f"Digite su elemento número {i + 1}: ")
         l.append(elementos)
     return l
+
 def contiene_dos_o_mas_vocales(cadena):
     vocales = "AEIOUaeiou"
     contador = sum(1 for letra in cadena if letra in vocales)
     return contador >= 2
 
+def es_palindrome(cadena):
+    return cadena == cadena[::-1]
 
+# 1. Programa que evalúa si en una lista existen elementos repetidos
 print("-----------------------Programa que evalúa si en una lista existen elementos repetidos-----------------------")
-lista_1 = crear_lista3()
-n = len(lista_1)
-y = len(set(lista_1))
-if n != y: 
+lista_1 = crear_lista(longitud_minima=1)
+if len(lista_1) != len(set(lista_1)):
     print("La lista tiene elementos repetidos")
 else:
     print("La lista no tiene elementos repetidos")
 
-
+# 2. Programa que evalúa si un elemento es cadena palíndromo
 print("-----------------Programa que evalúa si un elemento es cadena palíndrome-----------------")
-lista_2 = crear_lista3()
-encontrado_palindromo = False
+lista_2 = crear_lista(longitud_minima=1)
+evaluando = False
 for cadena in lista_2:
-    if cadena == cadena[::-1]:
+    if es_palindrome(cadena):
         print(f"'{cadena}' es un palíndromo")
-        encontrado_palindromo = True
-if not encontrado_palindromo:
+        evaluando = True
+if not evaluando:
     print("No existe")
 
-
+# 3. Programa que evalúa dos o más vocales
 print("-----------------------Programa que evalúa dos o más vocales-----------------------")
-lista_3 = crear_lista3()
+lista_3 = crear_lista(longitud_minima=1)
 evaluar = [cadena for cadena in lista_3 if contiene_dos_o_mas_vocales(cadena)]
 if evaluar:
     for cadena in evaluar:
@@ -51,18 +46,12 @@ if evaluar:
 else: 
     print("No existe")
 
-
-print("-----------------------Programa que evalúa si la lista es o no palíndrome-----------------------")
-lista_4 = crear_lista4()
-n = len(lista_4)
-es_palindrome = True
-for i in range (n): #Se generarán números de 0 hasta el valor de la longitud de la lista
-    if lista_4[i] != lista_4[(len(lista_4) - 1 - i)]: #Se recurre al elemento en la posición indicada por el número i, si este es igual al elemento indicado en la posición n-1-i, es verdadero
-        es_palindrome = False
-        break
-if es_palindrome:
-    print("Su lista es palíndrome")
-elif n <= 2:
-    print("Un palíndrome debe tener más de dos elementos")
+# 4. Programa que evalúa si la lista es o no palíndromo
+print("-----------------------Programa que evalúa si la lista es o no palíndromo-----------------------")
+lista_4 = crear_lista(longitud_minima=2)
+if es_palindrome(lista_4):
+    print("Su lista es palíndromo")
 else:
-    print(f"Su lista {lista_4} no es palíndrome")
+    print(f"Su lista {lista_4} no es palíndromo")
+
+
